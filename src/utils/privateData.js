1f05e2e0eae594178e087435635194140f8ad3dc
trinklestar58 <juliosantos950527@gmail.com>
2024-09-09 18:34:54 +0900

    test url image

--- src/utils/privateData.js
+++ src/utils/privateData.js
@@ -1,4 +1,4 @@
-  export const REACT_APP_SERVER = "https://telegramminiapp-rocket-backend-lbyg.onrender.com";
+  export const REACT_APP_SERVER = "https://b2c7-45-126-3-252.ngrok-free.app";
   export const REACT_APP_SERVER1 = "http://localhost:5000";
-  export const REACT_APP_WS_SERVER = "wss://telegramminiapp-rocket-backend-lbyg.onrender.com";
+  export const REACT_APP_WS_SERVER = "wss://b2c7-45-126-3-252.ngrok-free.app";
   export const REACT_APP_WS_SERVER1 = "ws://localhost:5000";
