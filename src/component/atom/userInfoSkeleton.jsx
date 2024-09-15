00899220504f3e7594fb03cd4d88f8181f7e3fec
trinklestar58 <juliosantos950527@gmail.com>
2024-09-16 04:54:35 +0900

    fixed all skeleton

--- src/component/atom/userInfoSkeleton.jsx
+++ src/component/atom/userInfoSkeleton.jsx
@@ -15,7 +15,7 @@ const UserInfoSkeleton = () => {
       <div className="flex animate-pulse  items-center gap-2 text-[14px] font-medium">
         <div className="rounded-full bg-gray-300 h-6 w-6"></div>
         <div>
-          <div className="h-6 w-8 bg-gray-300 rounded"></div>
+          <div className="h-3.5 w-8 bg-gray-300 rounded"></div>
         </div>
       </div>
 
