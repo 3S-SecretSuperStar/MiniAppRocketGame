d9b72cc51e0d0def8bd9ecf33803e9531d8b84a5
trinklestar58 <juliosantos950527@gmail.com>
2024-09-16 04:45:22 +0900

    add skeleton 2x

--- src/component/atom/userInfoSkeleton.jsx
+++ src/component/atom/userInfoSkeleton.jsx
@@ -5,9 +5,9 @@ const UserInfoSkeleton = () => {
       <div className="animate-pulse flex gap-1 items-center">
         <div className="rounded-full bg-gray-200 h-8 w-8"></div>
         <div className="flex flex-col text-[14px] text-white font-bold">
-          <div className="h-6 w-52 bg-gray-200 rounded"></div>
-          <div className="h-6 w-52 bg-gray-200 rounded"></div>
-          <div className="h-6 w-52 bg-gray-200 rounded"></div>
+          <div className="h-4 w-48 bg-gray-200 rounded"></div>
+          <div className="h-4 w-52 bg-gray-200 rounded"></div>
+          <div className="h-4 w-12 bg-gray-200 rounded"></div>
 
         </div>
       </div>
@@ -15,7 +15,7 @@ const UserInfoSkeleton = () => {
       <div className="flex animate-pulse  items-center gap-2 text-[14px] font-medium">
         <div className="rounded-full bg-gray-200 h-6 w-6"></div>
         <div>
-          <div className="h-6 w-10 bg-gray-200 rounded"></div>
+          <div className="h-6 w-8 bg-gray-200 rounded"></div>
         </div>
       </div>
 
