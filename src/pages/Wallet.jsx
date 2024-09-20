190562f0c29f543c68db4fea979579fc457e1716
trinklestar58 <juliosantos950527@gmail.com>
2024-09-21 00:36:31 +0900

    add tonconnect ui

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import InfoModal from "../component/atom/infoModel";
 import AtomLabel from "../component/atom/atom-label";
 import ShadowButton from "../component/atom/shadow-btn";
@@ -7,30 +7,43 @@ import Contact from "../component/molecules/contact";
 import { useAtom } from "jotai";
 import { isActionState } from "../store";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { useTonConnectUI, useTonWallet } from "@tonconnect/ui-react";
+import { Img } from "../assets/image";
+import NavWallet from "../component/svg/nav_wallet";
+import CheckMark from "../component/svg/check-mark";
+import WalletInfo from "../component/atom/wallet-info";
+import InputNumber from "../component/template/InputNumber";
 
 
 const Wallet = () => {
 
   // const [walletAddress, setWalletAddress] = useState("");
   const [infoState, setInfoState] = useState(false)
-  const[,setActionState]= useAtom(isActionState);
+  const [, setActionState] = useAtom(isActionState);
+  const [tx, setTx] = useState({})
+  const wallet = useTonWallet();
+  const [tonconnectUi] = useTonConnectUI();
+  const [tokenNumber, setTokenNumber] = useState(1000);
+
+  const onChange = useCallback((value) => setTx(value.updated_src), [])
+
   setActionState('stop')
   return (
     <div className="h-full pb-[76px] flex flex-col">
       <div className="flex-auto flex" style={{ height: "calc(100vh - 320px)" }}>
         <div className="my-auto flex flex-col items-center text-center gap-4 h-fit">
-          
+
           <LazyLoadImage
-                alt="wallet"
-                effect="opacity"
-                wrapperProps={{
-                  style: {
-                    transitionDelay: "1s",
-                    maxHeight: "auto",
-                    maxWidth: '200px'
-                  },
-                }}
-                src="/image/main/ton.png" />
+            alt="wallet"
+            effect="opacity"
+            wrapperProps={{
+              style: {
+                transitionDelay: "1s",
+                maxHeight: "auto",
+                maxWidth: '200px'
+              },
+            }}
+            src="/image/main/ton.png" />
           <div className="text-[15px] text-white">
             Connect your Token wallet to be able to play with Token and receive rewards from the platform.
           </div>
@@ -38,13 +51,29 @@ const Wallet = () => {
       </div>
       <div className="w-full">
         <AtomLabel content={"Wallet"} />
+        <WalletInfo address={wallet}/>
+        {
+          wallet &&
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-1">
+            Buy coins
+            <InputNumber InputProps = {{value:tokenNumber,min:1,onchange: e=>setTokenNumber(e.target.value)}} />
+            1TON = 100 coins
+            </div>
+          </div>
+              
+            
+
+
+        }
         {/* <WalletInfo className={"mt-2"} address={walletAddress} /> */}
-        <ShadowButton
-          className={"mt-4"}
-          content="Connect wallet"
-          // content={walletAddress ? "Disconnect wallet" : "Connect wallet"}
-          action={() => setInfoState(true)}
-        />
+
+            <ShadowButton
+                className={`mt-4 ${wallet?'bg-[#CC070A]':'bg-[#3434DA]'}`}
+                content={wallet ? "Disconnect wallet" : "Connect wallet"}
+                action={() => setInfoState(true)}
+              />
+
       </div>
       <InfoModal title="Coming soon!" isOpen={infoState} setIsOpen={() => setInfoState(false)} height="h-[280px]">
         <div className="flex items-center justify-center">
@@ -58,7 +87,7 @@ const Wallet = () => {
             📢 Join our social media to stay up to date.
           </div>
 
-          <Contact/>
+          <Contact />
         </div>
 
       </InfoModal>
