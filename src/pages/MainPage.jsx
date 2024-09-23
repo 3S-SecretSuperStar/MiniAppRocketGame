65f239ef07139b2c1af0b497c74cdd39a1746eac
trinklestar58 <juliosantos950527@gmail.com>
2024-09-23 18:25:10 +0900

    test 5: fetch

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -278,7 +278,7 @@ const MainPage = () => {
               .then(res => Promise.all([res.status, res.json()]))
               .then(([status, data]) => {
                 try {
-                  if(gamePhase!=='started')
+                  if(gamePhase !== 'started' )
                  { // console.log(data)
                   // console.log(realName)
                   // console.log(data.userData)
@@ -349,7 +349,7 @@ const MainPage = () => {
 
     }
     fetchData()
-  }, [isReal, gamePhase])
+  }, [])
   if (loading && firstLoading) {
     setActionState("start")
     return <FetchLoading />
