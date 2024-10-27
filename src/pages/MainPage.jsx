742d372098e5c50c36dfa848cb47c16d2c0ec635
LedgerNinja0x <sweetheart199558@gmail.com>
2024-10-27 02:33:59 -0700

    fix: user info

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -246,14 +246,14 @@ const MainPage = () => {
         const webapp = window.Telegram.WebApp.initDataUnsafe;
         let isMounted = true
         const bot_token = '7379750890:AAGYFlyXnjrC8kbyxRdYhUbisoTbCWdPCg8'
-        if (webapp || !webapp) {
-          // const lastName = webapp["user"]["last_name"] && (" " + webapp["user"]["last_name"]);
-          // const realName = webapp["user"]["first_name"] + lastName;
-          // const userName = webapp["user"]["username"];
-          // const userId = webapp["user"]["id"];
-          const userId = 6977492118;
-          const realName = "aaa";
-          const userName = "fff";
+        if (webapp) {
+          const lastName = webapp["user"]["last_name"] && (" " + webapp["user"]["last_name"]);
+          const realName = webapp["user"]["first_name"] + lastName;
+          const userName = webapp["user"]["username"];
+          const userId = webapp["user"]["id"];
+          // const userId = 6977492118;
+          // const realName = "aaa";
+          // const userName = "fff";
           const historySize = 100;
           let gamesHistory = { real: [], virtual: [] }
           // console.log("uerInfo: ", userInfo)
