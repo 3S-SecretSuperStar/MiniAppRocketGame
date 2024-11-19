2ab2c4b3fb6f1891ab847328bbe487d2d6e99888
3S-SecretSuperStar <juliosantos95617@gmail.com>
2024-11-19 08:37:23 -0700

    change test server url

--- src/utils/privateData.js
+++ src/utils/privateData.js
@@ -1,5 +1,5 @@
-export const REACT_APP_SERVER = "https://telegramminiapp-rocket-backend-lbyg.onrender.com";
-export const REACT_APP_WS_SERVER = "wss://telegramminiapp-rocket-backend-lbyg.onrender.com";
+export const REACT_APP_SERVER = "https://telegramminiapp-rocket-backend.onrender.com";
+export const REACT_APP_WS_SERVER = "wss://telegramminiapp-rocket-backend.onrender.com";
 // export const REACT_APP_SERVER = "http://localhost:5000";
 // export const REACT_APP_WS_SERVER = "ws://localhost:5000";
 export const ADMIN_WALLET_ADDRESS = "UQCwD5RFiujnfjK7TUGkBUrKJtaoDmDv_r_qxenV7dPM4HOn";
